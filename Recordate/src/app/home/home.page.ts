0607123362afae22be0bb9fa3afb356e604064fb
jorge manriquez <jor.manriquez@duocuc.cl>
2023-09-03 19:10:29 -0300

    creacion de registro

--- Recordate/src/app/home/home.page.ts
+++ Recordate/src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-
+import{
+  FormGroup,
+  FormControl,
+  Validator,
+  FormBuilder
+} from '@angular/forms'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
