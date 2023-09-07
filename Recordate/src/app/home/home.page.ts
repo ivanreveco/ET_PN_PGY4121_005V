6bc5b1ecd91509560fc243438f799f9c3ad3bbcf
jorge manriquez <jor.manriquez@duocuc.cl>
2023-09-07 17:05:07 -0300

    creacion de login/registro con modal

--- Recordate/src/app/home/home.page.ts
+++ Recordate/src/app/home/home.page.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  name='';
+  correo_ingreso ='';
+  contrasena_ingreso = '';
+  name = '';
 
   constructor() {}
+
+  ngOnInit(){}
 
   enviar() {
     // Código para enviar el nombre
@@ -24,5 +27,26 @@ export class HomePage {
   enviarName() {
     // Código para enviar el nombre al servidor
   }
+
+  login(){
+
+    if(this.correo_ingreso === '' || this.contrasena_ingreso === ''){
+      this.limpiarCampos();
+    }
+    
+  }
+  limpiarCampos(){
+    this.contrasena_ingreso = ' ';
+    this.correo_ingreso = ' ';
+  }
+  validarIngreso() : boolean {
+
+    if (this.contrasena_ingreso === '' || this.correo_ingreso === '') {return false;}
+    return true;
+    
+  }
+
+ 
+    
 
 }
