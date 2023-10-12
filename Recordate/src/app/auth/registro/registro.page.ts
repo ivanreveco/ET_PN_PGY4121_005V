952de4dee9655b1c463b194f46f1f6f8189df611
ivan <sw65ivan@gmail.com>
2023-10-12 13:44:54 -0300

    arreglo error de registro y redireccion

--- Recordate/src/app/auth/registro/registro.page.ts
+++ Recordate/src/app/auth/registro/registro.page.ts
@@ -1,85 +1,79 @@
 import { FirebaseService } from '../../services/firebase.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {UtilsService, } from '../../services/utils.service'
+import { UtilsService } from '../../services/utils.service';
 import { User } from 'src/app/models/user.models';
 import { customValidators } from 'src/app/util/custom-validators';
-import { Router} from '@angular/router';
-
-
-
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage implements OnInit  {
- 
-
-  form=new FormGroup({
-    email: new FormControl('',[Validators.required, Validators.email]),
-    password: new FormControl('',[Validators.required, Validators.minLength(7)]),
-    name: new FormControl('',[Validators.required, Validators.minLength(4)]),
-    lastName:new FormControl('',[Validators.required, Validators.minLength(4)]),
+export class RegistroPage implements OnInit {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(7)]),
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    lastName: new FormControl('', [Validators.required, Validators.minLength(4)]),
     confirmPasword: new FormControl(''),
+  });
 
-  })
- 
-  confirmPasswordV(){
-    this.form.controls.confirmPasword.setValidators([Validators.required,
-    customValidators.machValues(this.form.controls.password) 
-    ])
-    this.form.controls.confirmPasword.updateValueAndValidity();
-  }
-  constructor( private firebaseSvc :FirebaseService,
-  private utilsSvc: UtilsService,
-  private router: Router){
-   
-  }
+  constructor(
+    private firebaseSvc: FirebaseService,
+    private utilsSvc: UtilsService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.confirmPasswordV()
+    this.confirmPasswordV();
   }
-  redirectToHomePage() {
-    this.router.navigateByUrl('/home-page');
+
+  confirmPasswordV() {
+    this.form.controls.confirmPasword.setValidators([
+      Validators.required,
+      customValidators.machValues(this.form.controls.password),
+    ]);
+    this.form.controls.confirmPasword.updateValueAndValidity();
   }
+
   submit() {
     if (this.form.valid) {
-      this.utilsSvc.presentLoading({message: 'Registrando...', duration: 1000 });
-      this.firebaseSvc.SignUp(this.form.value as User).then(async res => {
-        console.log(res);
+      this.utilsSvc.presentLoading({ message: 'Registrando...', duration: 1000 });
+      this.firebaseSvc.SignUp(this.form.value as User).then(
+        async (res) => {
+          this.form.reset();
+          this.redirectToHomePage(); 
+          console.log(res);
 
-        await this.firebaseSvc.updateUser({displayName: this.form.value.name})
-        let user: User = {
-          uid: res.user.uid,
-          name: res.user.displayName,
-          email:res.user.email,
-          lastName:this.form.value.lastName
+          await this.firebaseSvc.updateUser({ displayName: this.form.value.name });
+          let user: User = {
+            uid: res.user.uid,
+            name: res.user.displayName,
+            email: res.user.email,
+            lastName: this.form.value.lastName,
+          };
+
+          this.utilsSvc.setElementeInStorage('user', user);
+          this.utilsSvc.dismissloading();
+
+         
+        },
+        (error) => {
+          this.utilsSvc.dismissloading();
+          this.utilsSvc.presentToast({
+            message: 'Correo ya existe',
+            duration: 5000,
+            color: 'warning',
+            icon: 'alert-circle-outline',
+          });
         }
-
-        this.utilsSvc.setElementeInStorage('user', user);
-        this.utilsSvc.dismissloading();
-
-        this.utilsSvc.presentToast({
-          message: `Te damos la bienvenida ${user.name}`,
-          duration: 1500,
-          color: 'primary',
-          icon: 'person-outline',
-          mode: 'ios'
-        });
-        this.form.reset();
-      }, error => {
-        this.utilsSvc.dismissloading();
-        this.utilsSvc.presentToast({
-          message: error.message,
-          duration: 5000,
-          color: 'warning',
-          icon: 'alert-circle-outline'
-        });
-      });
+      );
     }
   }
 
+  private redirectToHomePage() {
+    this.router.navigateByUrl('/home-page');
+  }
 }
--- Recordate/src/app/services/utils.service.ts
+++ Recordate/src/app/services/utils.service.ts
@@ -1,34 +1,49 @@
 import { Injectable } from '@angular/core';
 import { LoadingController, LoadingOptions, ToastController, ToastOptions } from '@ionic/angular';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilsService {
+  private errorMessageShown: boolean = false;
 
-  constructor(private loadingController: LoadingController,
-    private router :Router,
-    private toastController: ToastController ) {}
-   async presentLoading(opts: LoadingOptions) {
+  constructor(private loadingController: LoadingController, private router: Router, private toastController: ToastController) {}
+
+  async presentLoading(opts: LoadingOptions) {
     const loading = await this.loadingController.create(opts);
     await loading.present();
-   }
-   async dismissloading(){
-    return await this.loadingController.dismiss()
-   }
-   setElementeInStorage(key: string, element: any){
-    return localStorage.setItem(key, JSON.stringify(element))
-   }
-   getElementInStorage(key: string){
-    return JSON.parse(localStorage.getItem(key))
-   }
-   async presentToast(opts:ToastOptions) {
+  }
+
+  async dismissloading() {
+    return await this.loadingController.dismiss();
+  }
+
+  setElementeInStorage(key: string, element: any) {
+    return localStorage.setItem(key, JSON.stringify(element));
+  }
+
+  getElementInStorage(key: string) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  async presentToast(opts: ToastOptions) {
     const toast = await this.toastController.create(opts);
     toast.present();
-   }
 
-   RouterLink(url : string){
+    // Set errorMessageShown to true when displaying a toast
+    this.errorMessageShown = true;
+  }
+
+  isErrorMessageShown() {
+    return this.errorMessageShown;
+  }
+
+  resetErrorMessageShown() {
+    this.errorMessageShown = false;
+  }
+
+  RouterLink(url: string) {
     return this.router.navigateByUrl(url);
-   }
+  }
 }
