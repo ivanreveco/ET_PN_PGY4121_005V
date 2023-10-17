132cdb349d893c6827ef7e0875d9bc0e1671a500
ivan <sw65ivan@gmail.com>
2023-10-17 02:56:11 -0300

    a

--- Recordate/src/app/note/note.page.ts
+++ Recordate/src/app/note/note.page.ts
@@ -9,21 +9,9 @@ import { FirebaseService } from '../services/firebase.service';
   styleUrls: ['./note.page.scss'],
 })
 export class NotePage implements OnInit {
-@Input() note: Note;
-newNote: Note = {
-  id: '',
-  nombreNota: '',
-  descripcion: '',
-  tiponota: '',
-};
 
-  form = new FormGroup({
-    nombreNota:new FormControl('',[Validators.required]),
-    descripcion:new FormControl('',[Validators.required]),
-    Tiponota:new FormControl('')
-   
 
-  })
+ 
 
   constructor(
     private firebaseSvc:FirebaseService
@@ -31,14 +19,4 @@ newNote: Note = {
 
   ngOnInit() {
   }
-  save(){
-    console.log('Esto se envia a firebase ' ,this.newNote)
-    const data = this.newNote;
-    data.id = this.firebaseSvc.createId();
-    const enlace = 'user';
-    this.firebaseSvc.set_User<Note>(data, enlace, data.id);
-    console.log(data,enlace,'Hola');
-
-  }
-
 }
